19dcaf36a7cb7a56df6f21ce3f42969b2cb5bbbe
ardaboyroo <97671519+ardaboyroo@users.noreply.github.com>
2022-01-13 19:58:01 +0100

    Set main "base"

--- main.py
+++ main.py
@@ -2,19 +2,20 @@ import pygame
 import math
 import random
 pygame.init()
+IsRunning = True #Een boolean voor de while loop
 
 win = pygame.display.set_mode((1220, 800))
 
 pygame.display.set_caption("Arda en Nieks reetro Arkade")
 
-IsRunning = True
-
 while IsRunning:
     win.fill((110, 50, 205))
-    pygame.time.delay(30) #Framerate in 1/milliseconden
+    pygame.time.delay(33) #Framerate in 1/milliseconden
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             IsRunning = False
-
+    
+    
+    
     pygame.display.update()
 pygame.quit()
